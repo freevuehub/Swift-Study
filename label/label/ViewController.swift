e8b324bc98a261d0fa9bef1ca3bd5ad87d24b5af
홍성준 <doo9392@bemycar.co.kr>
2019-05-10 23:41:25 +0900

    Label 프로그래밍 적 디자인

---
+++ label/label/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  label
+//
+//  Created by 홍성준 on 10/05/2019.
+//  Copyright © 2019 freevue. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
