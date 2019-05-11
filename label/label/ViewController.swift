85c2ecce99bc16daa7a40cb4975c325cc3be03b6
홍성준 <doo9392@bemycar.co.kr>
2019-05-11 09:55:22 +0900

    Label 프로그래밍 적 디자인

--- label/label/ViewController.swift
+++ label/label/ViewController.swift
@@ -10,17 +10,28 @@ import UIKit
 
 class ViewController: UIViewController {
     @IBOutlet weak var myLabel: UILabel!
+    @IBOutlet weak var label1: UILabel!
+    @IBOutlet weak var label2: UILabel!
+    @IBOutlet weak var label3: UILabel!
+    @IBOutlet weak var label4: UILabel!
+    @IBOutlet weak var label5: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
-        myLabel.text = "Hello World";
+        myLabel.text = "Hello Label World";
         myLabel.textColor = UIColor.blue;
         myLabel.backgroundColor = UIColor.black;
         myLabel.textAlignment = NSTextAlignment.center;
         myLabel.numberOfLines = 0;
         myLabel.font = UIFont.systemFont(ofSize: 20);
+        
+        label1.textColor = UIColor.blue;
+        label2.backgroundColor = UIColor.black;
+        label3.textAlignment = NSTextAlignment.center;
+        label4.numberOfLines = 0;
+        label5.font = UIFont.systemFont(ofSize: 20);
     }
 
 
