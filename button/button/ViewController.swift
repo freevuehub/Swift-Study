418deb6371b7c1e94f6814a906046bf1dfba11c0
홍성준 <hackish@hanmail.net>
2019-05-12 21:23:24 +0900

    Button 여러 설정 테스트

--- button/button/ViewController.swift
+++ button/button/ViewController.swift
@@ -10,12 +10,16 @@ import UIKit
 
 class ViewController: UIViewController {
     @IBOutlet weak var myBtn: UIButton!
+    @IBOutlet weak var disavled: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
         myBtn.setTitle("눌러 주세요.", for: UIControl.State.normal);
+        myBtn.setTitle("누르는 중이네요.", for: UIControl.State.highlighted);
+        
+        disavled.isEnabled = false;
     }
 
 
