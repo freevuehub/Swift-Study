b7a3809cb707539595f21b73108431c7d4ef39e9
홍성준 <doo9392@bemycar.co.kr>
2019-05-12 19:45:21 +0900

    Button 프로젝트 생성

--- button/button/ViewController.swift
+++ button/button/ViewController.swift
@@ -9,7 +9,8 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var myBtn: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
