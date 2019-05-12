0c263ebaf2fcc00f52ce18a3c380bd83f0207783
홍성준 <hackish@hanmail.net>
2019-05-12 21:09:57 +0900

    Button setTitle()

--- button/button/ViewController.swift
+++ button/button/ViewController.swift
@@ -14,6 +14,8 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        myBtn.setTitle("눌러 주세요.", for: UIControl.State.normal);
     }
 
 
