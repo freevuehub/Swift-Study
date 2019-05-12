18d7c3a035d040f4b6ab1a0890850fd36c25bc8a
홍성준 <hackish@hanmail.net>
2019-05-12 21:29:47 +0900

    Button Action Test

--- button/button/ViewController.swift
+++ button/button/ViewController.swift
@@ -8,9 +8,16 @@
 
 import UIKit
 
+var btnView:Bool = true;
+
 class ViewController: UIViewController {
     @IBOutlet weak var myBtn: UIButton!
     @IBOutlet weak var disavled: UIButton!
+    @IBAction func btnAction() {        
+        btnView = !btnView;
+        
+        viewDidLoad();
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,9 +26,7 @@ class ViewController: UIViewController {
         myBtn.setTitle("눌러 주세요.", for: UIControl.State.normal);
         myBtn.setTitle("누르는 중이네요.", for: UIControl.State.highlighted);
         
-        disavled.isEnabled = false;
+        
+        disavled.isEnabled = btnView;
     }
-
-
 }
-
