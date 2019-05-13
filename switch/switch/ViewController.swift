b5be1394f679c873d8f88551524ed4297d105d64
홍성준 <hackish@hanmail.net>
2019-05-13 21:09:11 +0900

    스위치와 버튼 테스트

--- switch/switch/ViewController.swift
+++ switch/switch/ViewController.swift
@@ -7,12 +7,39 @@
 //
 
 import UIKit
+var abled: Bool = false;
+var color: UIColor = UIColor.red;
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var btn: UIButton!
+    @IBOutlet weak var mySwitch: UISwitch!
+    
+    @IBAction func switchAction() {
+        abled = !abled;
+        
+        make();
+    }
+    @IBAction func btnAction() {
+        color = UIColor.blue;
+        
+        setColor();
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        make();
+        setColor();
+    }
+    
+    func make() {
+        mySwitch.isOn = abled;
+        btn.isEnabled = abled;
+    }
+    
+    func setColor() {
+        mySwitch.onTintColor = color;
     }
 
 
